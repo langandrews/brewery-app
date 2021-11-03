5452fa85338e96a1d0d11df4c3b6f7035b791cb9
stephenvsp <stephenvsp@gmail.com>
2021-11-03 11:12:31 -0400

    add api call to brewery db

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React, { FormEvent, SyntheticEvent, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useInput } from "./hooks/input-hook";
+import { Brewery } from "./types/Brewery";
 import "./App.css";
+import axios from "axios";
 
 export default function App() {
   const { value: state, bind: bindState, reset: resetState } = useInput("");
   const { value: city, bind: bindCity, reset: resetCity } = useInput("");
+  const [breweries, setBreweries] = useState<Brewery[]>([]);
+
+  let breweryList = breweries.map(brewery =>
+    <li>{brewery.name}</li>
+  )
+  
   const handleBrewerySearch = (event: FormEvent) => {
     event.preventDefault();
+    let stateQueryParameter = "by_state=" + state
+    let cityQueryParameter = "by_city=" + city
+    axios.get<Brewery[]>("https://api.openbrewerydb.org/breweries?" + stateQueryParameter + "&" + cityQueryParameter)
+    .then(response => {
+        setBreweries(response.data)
+      }
+    )
   };
 
   return (
@@ -33,6 +48,9 @@ export default function App() {
           <input type="submit" value="Submit" />
         </div>
       </form>
+      <ul>
+        {breweryList}
+      </ul>
     </div>
   );
 }
---
+++ src/types/Brewery.tsx
@@ -0,0 +1,19 @@
+export interface Brewery {
+    id: Number,
+    name: String,
+    brewery_type: String,
+    street: String,
+    address_2: String,
+    address_3: String,
+    city: String,
+    state: String,
+    county_province: String,
+    postal_code: String,
+    country: String,
+    longitude: String,
+    latitude: String,
+    phone: String,
+    website_url: String,
+    updated_at: String,
+    created_at: String
+  }
