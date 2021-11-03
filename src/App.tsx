1e2a79f511f71416215840f7d35b0219eea82e8c
stephenvsp <stephenvsp@gmail.com>
2021-11-03 13:47:49 -0400

    refactor form into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,24 @@ import { useInput } from "./hooks/input-hook";
 import { Brewery } from "./types/Brewery";
 import "./App.css";
 import axios from "axios";
+import BreweryForm from "./components/BreweryForm";
 
 export default function App() {
-  const { value: state, bind: bindState, reset: resetState } = useInput("");
-  const { value: city, bind: bindCity, reset: resetCity } = useInput("");
   const [breweries, setBreweries] = useState<Brewery[]>([]);
 
   let breweryList = breweries.map(brewery =>
     <li>
       <a href={brewery.website_url} target="_blank">{brewery.name}</a>
+      <p>{brewery.phone}</p>
+      <p>{brewery.latitude}</p>
+      <p>{brewery.longitude}</p>
     </li>
   )
   
-  const handleBrewerySearch = (event: FormEvent) => {
-    event.preventDefault();
-    let stateQueryParameter = "by_state=" + state
-    let cityQueryParameter = "by_city=" + city
-    axios.get<Brewery[]>("https://api.openbrewerydb.org/breweries?" + stateQueryParameter + "&" + cityQueryParameter)
-    .then(response => {
-        setBreweries(response.data)
-      }
-    )
-  };
 
   return (
     <div className="App">
-      <form onSubmit={handleBrewerySearch}>
-        <div>
-          <label>
-            City:
-            <div>
-              <input type="text" {...bindCity} />
-            </div>
-          </label>
-        </div>
-        <div>
-          <label>
-            State:
-            <div>
-              <input type="text" {...bindState} />
-            </div>
-          </label>
-        </div>
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
+      <BreweryForm setBreweriesCallback={setBreweries}/>
       <ul>
         {breweryList}
       </ul>
